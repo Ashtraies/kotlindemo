0cf1e75dcf4ca7f7eb3ba2e9fac388747b74dd3a
442109211@qq.com <442109211@qq.com>
2018-06-23 12:42:13 +1200

    first commit and initialize remote repository

---
+++ app/src/main/java/org/ashtray/mobile/kotlin/demo/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.ashtray.mobile.kotlin.demo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
